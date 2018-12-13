656b2ce2f6be4641d6608eb03b83ef5f4185b5c2
mkawa95 <dahabusaidi@gmail.com>
2018-12-13 19:03:15 +0300

    added new  components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
 
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+
+ // importing components
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
+import { StudentDetailsComponent } from './student-details/student-details.component';
+import { FooterComponent } from './footer/footer.component';
+
+ // Defining routes
+const AppRoutes: Routes = [
+    { path: 'home' , component: HomeComponentComponent},
+    { path: 'reset-password', component: PasswordResetComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+      AppComponent,
+      LoginComponentComponent,
+      HomeComponentComponent,
+      PasswordResetComponent,
+      StudentDetailsComponent,
+      FooterComponent,
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      RouterModule.forRoot(AppRoutes),
+      BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
