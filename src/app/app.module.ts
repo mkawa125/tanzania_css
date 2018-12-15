27f04e2534a121ae4f962c8c45539b62b2927fea
mkawa95 <dahabusaidi@gmail.com>
2018-12-15 20:31:05 +0300

    added updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,22 @@ import { RouterModule, Routes} from '@angular/router';
 
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { SlickModule} from 'ngx-slick';
 
- // importing components
+// importing components
 import { LoginComponentComponent } from './login-component/login-component.component';
 import { HomeComponentComponent } from './home-component/home-component.component';
 import { PasswordResetComponent } from './password-reset/password-reset.component';
 import { StudentDetailsComponent } from './student-details/student-details.component';
 import { FooterComponent } from './footer/footer.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
  // Defining routes
 const AppRoutes: Routes = [
+    { path: '' , component: LoginComponentComponent},
     { path: 'home' , component: HomeComponentComponent},
     { path: 'reset-password', component: PasswordResetComponent},
 ];
@@ -26,13 +32,23 @@ const AppRoutes: Routes = [
       PasswordResetComponent,
       StudentDetailsComponent,
       FooterComponent,
+      MainComponent,
+      HeaderComponent,
   ],
   imports: [
       BrowserModule,
       RouterModule.forRoot(AppRoutes),
       BrowserAnimationsModule,
+      FormsModule,
+      ReactiveFormsModule,
+      SlickModule,
+      CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+    slides = [
+        {img: 'assets/images/africans.jpg'},
+    ];
+}
--- src/app/login-component/login-component.component.ts
+++ src/app/login-component/login-component.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login-component',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-component.component.css']
 })
 export class LoginComponentComponent implements OnInit {
-
-  constructor() { }
-
+  formData;
+  indexNumber;
+  constructor(private router: Router) { }
   ngOnInit() {
+      this.formData = new FormGroup({
+          indexNumber: new FormControl('', Validators.compose([
+              Validators.required,
+              Validators.maxLength(15),
+          ])),
+          password: new FormControl('', Validators.compose([
+              Validators.required,
+              Validators.minLength(6)])
+          )
+      });
+
+}
+  onClickSubmit(inputData) {
+    console.log(inputData.indexNumber);
+    if (inputData.indexNumber === 'admin' && inputData.password === '123456') {
+      // alert('login successfully');
+      this.router.navigate(['home']);
+        document.getElementById('login-container').style.display = '';
+        document.getElementById('home-container').style.display = '';
+    } else {
+        alert('Invalid Login');
+        return false;
+    }
   }
 
 }
