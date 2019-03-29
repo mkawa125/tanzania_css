ce3ec4a938b9c703f9a88d2ea8c8d901f622040d
dahabu saidi <dahabusaidi@gmail.com>
2019-03-29 16:39:18 +0300

    changed text-class to toast message

--- src/app/login-component/login-component.component.ts
+++ src/app/login-component/login-component.component.ts
@@ -36,7 +36,6 @@ export class LoginComponentComponent implements OnInit {
           this.toast.success('Login Successfully', 'Success', {
               timeOut: 2000,
               positionClass: 'toast-top-right',
-              messageClass: 'text-success',
           });
           this.router.navigate(['']);
           this.router.navigate(['home']);
