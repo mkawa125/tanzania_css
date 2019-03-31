e85d4683f60991fad055837052e56c545b663e0c
dahabu saidi <dahabusaidi@gmail.com>
2019-03-31 10:12:49 +0300

    added alt image to logo

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   constructor() { }
   tanzania_flag = '../assets/images/flag-tz.tmp';
-
+  no_image = 'No image';
   ngOnInit() {
   }
 
