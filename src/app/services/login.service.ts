c8d427a09febfca66944ea84f8b73885b738e8d9
dahabu saidi <dahabusaidi@gmail.com>
2019-03-31 12:54:16 +0300

    added login service

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  server = 'http://127.0.0.1:8000/api/v1/';
+  headers: Headers = new Headers();
+  options: any;
+
+  constructor( private http: Http) {
+    this.headers.append('enctype', 'multipart/form-data');
+    this.headers.append('Content-Type', 'application/json');
+    this.headers.append('X-requested-width', 'XMLHttpRequest');
+    this.options = new RequestOptions({headers: this.headers});
+  }
+}
