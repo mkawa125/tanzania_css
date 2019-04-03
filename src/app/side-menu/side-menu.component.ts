6955730a99e0e4a0dd386b561cfb174625f68797
dahabu saidi <dahabusaidi@gmail.com>
2019-04-04 00:54:21 +0300

    added staff view

--- src/app/side-menu/side-menu.component.ts
+++ src/app/side-menu/side-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginService} from '../services/login.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideMenuComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(
+      private loginService: LoginService,
+      private  router: Router,
+  ) { }
+  profile = '../assets/images/mkawa.jpg';
+  user_role = this.loginService.getUserROle();
+  username = this.loginService.getUserName();
   ngOnInit() {
   }
-
+  logout() {
+    this.loginService.logout().subscribe(
+        response => console.log(response),
+        error => console.log(error),
+        () => {
+          this.router.navigate(['/login']);
+        }
+    );
+  }
 }
