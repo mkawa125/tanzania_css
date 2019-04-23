13c5d3bdec1e2c87d0fa3299019105b09c152bd3
dahabu saidi <dahabusaidi@gmail.com>
2019-04-23 16:39:08 +0300

    contacts component working

--- src/app/contacts/contacts.module.ts
+++ src/app/contacts/contacts.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { ContactsRoutingModule } from './contacts-routing.module';
 import { ContactsComponent} from './contacts.component';
+import { NgxSpinnerModule} from 'ngx-spinner';
+import { SlickModule} from 'ngx-slick';
+import { ToastrModule} from 'ngx-toastr';
 
 @NgModule({
   declarations: [ContactsComponent],
   imports: [
-    CommonModule,
-    ContactsRoutingModule
+      CommonModule,
+      ContactsRoutingModule,
+      FormsModule,
+      ReactiveFormsModule,
+      ToastrModule.forRoot({
+        timeOut: 2000,
+        preventDuplicates: true,
+      }),
+      SlickModule,
+      NgxSpinnerModule,
   ]
 })
 export class ContactsModule { }
