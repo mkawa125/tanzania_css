84eeb7dd9982806d45741ddf676e9ad91a95723e
dahabu saidi <dahabusaidi@gmail.com>
2019-04-27 19:12:30 +0300

    add create contacts module

--- src/app/schools/create-school/create-school.component.ts
+++ src/app/schools/create-school/create-school.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, Validator, FormControl, FormBuilder, Validators} from '@angular/forms';
+import { ToastrService} from 'ngx-toastr';
+import { SchoolService} from '../../services/school.service';
+import { NgxSpinnerService} from 'ngx-spinner';
 
 @Component({
   selector: 'fmyp-create-school',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-school.component.css']
 })
 export class CreateSchoolComponent implements OnInit {
+  formData; name; regNumber;
 
-  constructor() { }
+  constructor(
+      private toast: ToastrService,
+      private schoolService: SchoolService,
+      private spinner: NgxSpinnerService,
+  ) { }
 
   ngOnInit() {
+    this.formData = new FormGroup({
+      regNumber: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.maxLength(20)
+      ])),
+      region: new FormControl('', Validators.compose([
+          Validators.required
+      ]))
+    });
   }
 
+  createSchool(formData) {
+    this.spinner.show();
+    console.log(formData);
+  }
 }
--- src/app/services/school.service.ts
+++ src/app/services/school.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SchoolService {
-
-  constructor() { }
+  server = 'http://127.0.0.1:8000/api/v1/';
+  headers: Headers = new Headers();
+  options: any;
+  constructor(private http: Http) {
+    this.headers.append('enctype', 'multipart/form-data');
+    this.headers.append('Content-Type', 'application/json');
+    this.headers.append('X-requested-width', 'XMLHttpRequest');
+    this.options = new RequestOptions({headers: this.headers});
+  }
+  createSchool(formData) {
+    const data = JSON.stringify(formData);
+    return this.http.post(this.server + 'createSchool', data, this.options).map(
+        res => res.json()
+    );
+  }
 }
