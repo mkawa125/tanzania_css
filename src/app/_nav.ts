842351820f77345e03a3e9881e471cf3bec73dc7
dahabu saidi <dahabusaidi@gmail.com>
2019-06-26 11:23:42 +0300

    remove dead links

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -65,80 +65,12 @@ export const navItems: NavData[] = [
     name: 'Students',
     url: '/buttons',
     icon: 'fa fa-graduation-cap',
-    children: [
-      {
-        name: 'Primary Students',
-        url: '/buttons/buttons',
-        icon: 'icon-cursor'
-      },
-      {
-        name: 'Secondary Students',
-        url: '/buttons/dropdowns',
-        icon: 'icon-cursor'
-      },
-      {
-        name: 'Advanced Students',
-        url: '/buttons/brand-buttons',
-        icon: 'icon-cursor'
-      }
-    ]
   },
   {
     name: 'Staffs',
     url: '/icons',
     icon: 'fa fa-user',
-    children: [
-      {
-        name: 'Non-Teachers',
-        url: '/icons/coreui-icons',
-        icon: 'icon-star',
-      },
-      {
-        name: 'Teachers',
-        url: '/icons/flags',
-        icon: 'icon-star'
-      },
-    ]
   },
-  // {
-  //   name: 'Exam Results',
-  //   url: '/notifications',
-  //   icon: 'fa fa-book',
-  //   children: [
-  //     {
-  //       name: 'Primary',
-  //       url: '/notifications/alerts',
-  //       icon: 'icon-bell'
-  //     },
-  //     {
-  //       name: 'O-level',
-  //       url: '/notifications/badges',
-  //       icon: 'icon-bell'
-  //     },
-  //     {
-  //       name: 'A-level',
-  //       url: '/notifications/modals',
-  //       icon: 'icon-bell'
-  //     }
-  //   ]
-  // },
-  // {
-  //   name: 'Admissions',
-  //   url: '/notifications',
-  //   icon: 'fa fa-tasks',
-  //   children: [
-  //     {
-  //       name: 'Primary',
-  //       url: '/notifications/alerts',
-  //       icon: 'icon-bell'
-  //     },
-  //     {
-  //       name: 'O-level',
-  //       url: '/notifications/badges',
-  //       icon: 'icon-bell'
-  //     },
-  //   ]
-  // },
   {
     divider: true
   },
@@ -151,21 +83,6 @@ export const navItems: NavData[] = [
     url: '/about',
     icon: 'fa fa-history'
   },
-  // {
-  //   name: 'Contacts',
-  //   url: '/contacts',
-  //   icon: 'fa fa-phone'
-  // },
-  // {
-  //   name: 'Setting',
-  //   url: '/charts',
-  //   icon: 'fa fa-cog'
-  // },
-  // {
-  //   name: 'my profile',
-  //   url: '/charts',
-  //   icon: 'fa fa-user-circle-o'
-  // },
   {
     name: 'Logout',
     url: '/login',
