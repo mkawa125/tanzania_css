f29781f46d93b6acb6e5cc97fe3eab9105176dde
dahabu saidi <dahabusaidi@gmail.com>
2019-08-26 21:55:26 +0300

    generate location service

--- src/app/locations/regions/regions.component.ts
+++ src/app/locations/regions/regions.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService} from 'ngx-spinner';
+import { Router} from '@angular/router';
+import { ToastrService} from 'ngx-toastr';
+import { LocationsService} from '../../services/locations.service';
+
+// declaring variable $ for data tables
+declare var $;
 
 @Component({
-  selector: 'fmyp-regions',
+  selector: 'app-regions',
   templateUrl: './regions.component.html',
   styleUrls: ['./regions.component.css']
 })
 export class RegionsComponent implements OnInit {
-
-  constructor() { }
-
+  regions: Array<any>;
+  public num = 1;
+  constructor(
+      private   route: Router,
+      private   spinner: NgxSpinnerService,
+      private   locaionService: LocationsService,
+      private toast: ToastrService,
+  ) {
+  }
   ngOnInit() {
+    this.spinner.show();
   }
 
 }
