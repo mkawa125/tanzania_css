e3b939e0b84f959636c1bf03bbdd83239a14f5a5
dahabu saidi <dahabusaidi@gmail.com>
2019-08-26 21:42:47 +0300

    fix region link error

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -102,7 +102,7 @@ export const routes: Routes = [
       },
       {
         path: 'regions',
-        loadChildren: './locations/locations.module#LocationsModule'
+        loadChildren: './locations/regions/regions.module#RegionsModule'
       },
     ]
   },
